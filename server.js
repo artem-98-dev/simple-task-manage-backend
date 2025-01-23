92e897075765828e857ac4a9cf78035ccb8a8ed9
artem-98-dev <honcharenkoaretem38@gmail.com>
2025-01-23 08:35:34 -0600

    fixed port issue

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 const tasksRouter = require("./routes/tasks");
 app.use("/api/tasks", tasksRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
